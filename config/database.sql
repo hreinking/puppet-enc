3c8a6ea8493f2d03a313fdfe79f6de4e66d878df
root <root@gs-puppet-master.slac.us.stanford.edu>
2019-05-14 19:39:14 +0000

    Add config folder

---
+++ config/database.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS Nodes;
+CREATE TABLE "Nodes" (
+	"node_definition"  TEXT UNIQUE NOT NULL Primary Key,
+	"cluster"  TEXT NOT NULL,
+	"datacenter"  TEXT NOT NULL,
+	"enclave"  TEXT NOT NULL,
+	"site"  TEXT NOT NULL,
+	"country"  TEXT NOT NULL,
+	"role"  TEXT NOT NULL,
+	"classes"  TEXT NOT NULL,
+	"environment"  TEXT NOT NULL
+);
+.separator ","
+.import config/nodes_database.csv Nodes
+DROP TABLE IF EXISTS TemporaryEnvironmentConfiguration;
+CREATE TABLE "TemporaryEnvironmentConfiguration" (
+	"fqdn"  TEXT UNIQUE NOT NULL Primary Key,
+	"environment"  TEXT NOT NULL,
+	"ConfTimeStart"  TEXT NOT NULL,
+	"ConfTimeEnd"  TEXT NOT NULL
+);
+.separator ","
+.import config/environments_database.csv TemporaryEnvironmentConfiguration
